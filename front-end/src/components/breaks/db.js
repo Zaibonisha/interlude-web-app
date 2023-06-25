b9d944d47b0c630e3ccd61cee5f241999c2aa9a7
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2023-06-25 02:55:10 +0100

    Add breaks cards

---
+++ front-end/src/components/breaks/db.js
@@ -0,0 +1,40 @@
+const CardData = [
+  {
+    id: 1,
+    description: 'Water color technique for calming your soul and take you to another place from the office.',
+    duration: '15 min',
+    category: 'medium',
+  },
+  {
+    id: 2,
+    description: 'Water color technique for calming your soul and take you to another place from the office.',
+    duration: '15 min',
+    category: 'medium',
+  },
+  {
+    id: 3,
+    description: 'Water color technique for calming your soul and take you to another place from the office.',
+    duration: '15 min',
+    category: 'medium',
+  },
+  {
+    id: 4,
+    description: 'Water color technique for calming your soul and take you to another place from the office.',
+    duration: '15 min',
+    category: 'medium',
+  },
+  {
+    id: 5,
+    description: 'Water color technique for calming your soul and take you to another place from the office.',
+    duration: '15 min',
+    category: 'medium',
+  },
+  {
+    id: 6,
+    description: 'Water color technique for calming your soul and take you to another place from the office.',
+    duration: '15 min',
+    category: 'medium',
+  },
+];
+
+export default CardData;
