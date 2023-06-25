80efe5a1b5574481424fe9b8fa571c44fd12414d
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2023-06-25 02:14:07 +0100

    Cleanup react default files

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Interlude Web App
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div> coming soon... </div>
   );
-}
+};
 
 export default App;
