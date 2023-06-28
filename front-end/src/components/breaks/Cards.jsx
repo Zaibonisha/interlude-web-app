8c9c7322e3e9b09756717a95cd473921255f6737
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2023-06-28 16:15:29 +0100

    Add cards components

---
+++ front-end/src/components/breaks/Cards.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import Heart from '../../assets/images/fav-heart.png';
+import ProfileImg from '../../assets/images/profile-pic.png';
+import CardData from './db';
+import '../../assets/styles/breaks/cards.css';
+
+const Cards = () => {
+  const data = CardData;
+  const [isClicked, setIsClicked] = useState(false);
+  const [clickedId, setClickedId] = useState(0);
+
+  const handleFavourite = (id) => {
+    data.map((item) => {
+      if (item.id === id) {
+        setIsClicked(!isClicked);
+        setClickedId(item.id);
+        console.log(id, isClicked, item.id, clickedId);
+      }
+    });
+  };
+
+  return (
+    <div className="text-[#fff] flex flex-row gap-10 card-container">
+      {data && data.map((item) => (
+        <ul key={item?.id} className="box">
+          <li className="card-bg-image flex flex-col justify-between">
+            <span className="flex flex-row justify-between">
+              {isClicked && item?.id === clickedId ? (
+                <FontAwesomeIcon className="fav-btn-1" onClick={() => handleFavourite(item?.id)} icon={faHeart} />
+              ) : (
+                <img className="fav-btn" src={Heart} alt={item?.category} onClick={() => handleFavourite(item?.id)} />
+              )}
+              <div className="flex flex-col gap-2 text-[#A7DAFF] text-[14px]">
+                <p className="duration-category text-center">{item?.duration}</p>
+                <p className="duration-category text-center">{item?.category}</p>
+              </div>
+            </span>
+            <div className="flex flex-row gap-5">
+              <img src={ProfileImg} alt={item?.category} className="profileImg" />
+              <p>Jessica Redman</p>
+            </div>
+          </li>
+          <li className="single-card">{item?.description}</li>
+          <li><button type="button" className="add-break-btn">ADD BREAK</button></li>
+        </ul>
+      ))}
+    </div>
+  );
+};
+
+export default Cards;
---
+++ front-end/src/components/breaks/Filters.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import "../../assets/styles/breaks/filters.css";
+import FilterData from "./filterDB";
+
+const Filters = () => {
+  return (
+    <div className="p-5 flex flex-col gap-5 text-[14px] w-[650px] m-auto filters">
+      <h2 className="text-[#FF1368]">Filters:</h2>
+      <ul>
+        {FilterData && FilterData.map((item) => (
+          <li key={item?.id} className="my-2">
+            <div className="flex flex-row gap-10">
+              <h3 className="text-[#8445E8]">{item?.title}:</h3>
+              <span className="flex flex-row gap-5">
+                <div className="">
+                  <input
+                    type="radio"
+                    name={item?.name}
+                    value={item?.value[0]}
+                    className="mr-1"
+                  />
+                  <label htmlFor={item?.value[0]} className="text-[#4D67C6]">
+                    {item?.value[0]}
+                  </label>
+                </div>
+                <div>
+                  <input
+                    type="radio"
+                    name={item?.name}
+                    value={item?.value[1]}
+                    className="mr-1"
+                  />
+                  <label htmlFor={item?.value[1]} className="text-[#4D67C6]">
+                    {item?.value[1]}
+                  </label>
+                </div>
+                {item?.value.length > 2 && (
+                  <div>
+                    <input
+                      type="radio"
+                      name={item?.name}
+                      value={item?.value[2]}
+                      className="mr-1"
+                    />
+                    <label htmlFor={item?.value[2]} className="text-[#4D67C6]">
+                      {item?.value[2]}
+                    </label>
+                  </div>
+                )}
+                {item?.value.length > 3 && (
+                  <div>
+                    <input
+                      type="radio"
+                      name={item?.name}
+                      value={item?.value[3]}
+                      className="mr-1"
+                    />
+                    <label htmlFor={item?.value[3]} className="text-[#4D67C6]">
+                      {item?.value[3]}
+                    </label>
+                  </div>
+                )}
+              </span>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Filters;
