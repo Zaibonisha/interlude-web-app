915c381966ec03660ccafe5e445af4f642375ed8
Zaibonisha <zsmohamed94@gmail.com>
2023-07-04 15:50:13 +0200

    Badges and Buttons

--- front-end/src/components/Sidebar.jsx
+++ front-end/src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './sidebar.css';
 import { FaHeartbeat } from "react-icons/fa";
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 import { TbActivityHeartbeat } from "react-icons/tb";
 import { MdSavedSearch, MdOutlineDashboard } from "react-icons/md";
 import { GiSettingsKnobs } from "react-icons/gi";
@@ -9,64 +9,74 @@ import { NavLink } from 'react-router-dom';
 import LogoutIcon from '@mui/icons-material/Logout';
 
 const Sidebar = ({ children }) => {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    const logout = () => {
-        localStorage.clear('token')
-        navigate('/sign-in')
-    }
-    const menuItem = [
-        {
-            path: "dashboard",
-            name: "Dashboard",
-            icon: <MdOutlineDashboard/>
-        },
-        {
-            path: "/breaks",
-            name: "Breaks",
-            icon: <MdSavedSearch />
-        },
-        {
-            path: "exercise",
-            name: "Exercise",
-            icon: <FaHeartbeat />
-        },
-        
-        {
-            path: "settings",
-            name: "Settings",
-            icon: <GiSettingsKnobs />
-        },
-       
-        
-    ];
+  const logout = () => {
+    localStorage.clear('token');
+    navigate('/sign-in');
+  };
 
-    return (
-        <div className="container">
-            <div className="sidebar">
-                <div className="top_section">
-                    <h1 className="logo"><img src="interlude-logo.png" alt="Logo" className="logo" style={{ width: '15vw',  }} /></h1>
-                    
-                </div>
-                {menuItem.map((item, index) => (
-                    <NavLink
-                        to={item.path}
-                        key={index}
-                        className="link"
-                        activeClassName="active"
-                    >
-                        <div className="icon">{item.icon}</div>
-                        <div className="link_text">{item.name}</div>
-                    </NavLink>
-                ))}
-                <img onClick={logout} src={LogoutIcon} />
-            </div>
-            <main>
-  <div>{children}</div>
-</main>
+  const menuItem = [
+    {
+      path: "dashboard",
+      name: "Dashboard",
+      icon: <MdOutlineDashboard/>
+    },
+    {
+      path: "/breaks",
+      name: "Breaks",
+      icon: <MdSavedSearch />
+    },
+    {
+      path: "exercise",
+      name: "Exercise",
+      icon: <FaHeartbeat />
+    },
+    {
+      path: "settings",
+      name: "Settings",
+      icon: <GiSettingsKnobs />
+    },
+  ];
 
+  return (
+    <div className="container">
+      <div className="sidebar">
+        <div className="top_section">
+          <h1 className="logo">
+            <img src="interlude-logo.png" alt="Logo" className="logo" style={{ width: '15vw' }} />
+          </h1>
         </div>
-    );
+        {menuItem.map((item, index) => (
+          <NavLink
+            to={item.path}
+            key={index}
+            className="link"
+            activeClassName="active"
+          >
+            <div className="icon">{item.icon}</div>
+            <div className="link_text">{item.name}</div>
+          </NavLink>
+        ))}
+        <button style={{
+              backgroundColor: 'blue',
+              color: 'white',
+              padding: '10px 20px',
+              border: 'none',
+              borderRadius: '5px',
+              cursor: 'pointer',
+              width: '15vw',
+              marginLeft: '10px'
+            }}onClick={logout} className="logout-button">
+          Logout
+        </button>
+      </div>
+      <main>
+        <div>{children}</div>
+      </main>
+    </div>
+  );
 };
 
 export default Sidebar;
+
