1d05090db943711ff7b9899c3b08f2920a6ca7a9
misgana96 <misganayoseph@gmail.com>
2023-07-07 20:07:04 +0300

    change component name

--- front-end/src/pages/Breaks.jsx
+++ front-end/src/pages/Breaks.jsx
@@ -2,11 +2,7 @@ import React, { useState } from 'react';
 import { Card, CardContent, Grid, Typography, AppBar, Toolbar, InputBase, Avatar } from '@mui/material';
 
 // import Navbar from "./Navbar";
-<<<<<<< HEAD
 import BreaksComp from '../components/breaks/index.jsx';
-=======
-import BreaksComp from '../components/breaks/Index.jsx';
->>>>>>> 11b4ca1ac15e5bd78656e1a4f96f68e95d95b736
 
 
 
